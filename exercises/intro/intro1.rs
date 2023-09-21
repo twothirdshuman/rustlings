c8628a5ba207423943da6f712394ffb04e05c722
twothirdshuman <75085645+twothirdshuman@users.noreply.github.com>
2023-09-21 16:34:47 +0000

    saving progress

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -13,14 +13,13 @@
 // Execute `rustlings hint intro1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     println!("Hello and");
     println!(r#"       welcome to...                      "#);
     println!(r#"                 _   _ _                  "#);
     println!(r#"  _ __ _   _ ___| |_| (_)_ __   __ _ ___  "#);
     println!(r#" | '__| | | / __| __| | | '_ \ / _` / __| "#);
+    println!(r"this is in the way LOL");
     println!(r#" | |  | |_| \__ \ |_| | | | | | (_| \__ \ "#);
     println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
     println!(r#"                               |___/      "#);
